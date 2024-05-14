f8d488d1565fc7a083240781c8fd75bda4df18f6
Duygu Hasan <duyguhsnhsn@gmail.com>
2024-05-14 20:58:30 +0300

    remove worker pool

--- main.go
+++ main.go
@@ -4,17 +4,10 @@ import (
 	"ccvalidator/api"
 	"log"
 	"net/http"
-	"runtime"
 )
 
 func main() {
-	jobs := make(chan api.Job, 100)
-
-	api.StartWorkerPool(runtime.NumCPU(), jobs)
-
-	http.HandleFunc("/validate", func(w http.ResponseWriter, r *http.Request) {
-		api.HandleRequest(jobs, w, r)
-	})
+	http.HandleFunc("/validate", api.HandleRequest)
 
 	log.Println("Server starting on port 8081...")
 	if err := http.ListenAndServe(":8081", nil); err != nil {
